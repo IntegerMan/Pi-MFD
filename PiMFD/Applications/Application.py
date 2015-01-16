93b629e32c3929f0521308e7c0f20056f35137bb
Matt Eland <Matt.Eland@Outlook.com>
2015-01-15 23:15:05 -0500

    Documented file.

--- PiMFD/Applications/Application.py
+++ PiMFD/Applications/Application.py
@@ -1,10 +1,17 @@
+# coding=utf-8
+"""
+Contains core application definition and placeholders.
+"""
 from PiMFD.Button import MFDButton
 
 __author__ = 'Matt Eland'
 
 
 class MFDApplication(object):
-
+    """
+    An abstract class representing the base of all applications.
+    :type controller: PiMFD.Controller.MFDController The controller
+    """
     pages = None
 
     active_page = None
@@ -17,6 +24,10 @@ class MFDApplication(object):
         self.pages = list()
 
     def get_buttons(self):
+        """
+        Gets the page buttons associated with the application.
+        :return: the page buttons associated with the application.
+        """
         buttons = list()
         for page in self.pages[0:self.controller.max_page_buttons]:  # TODO: Support paging for > max_page_buttons pages
             buttons.append(MFDButton(page.get_button_text(), selected=(self.active_page is page)))
@@ -24,24 +35,51 @@ class MFDApplication(object):
         return buttons
 
     def get_default_page(self):
+        """
+        Gets the default page.
+        :return: The default page.
+        """
         return None
 
     def get_button_text(self):
+        """
+        Gets the text used to render the button
+        :return: the text used to render the button
+        """
         return 'UNKN'
 
     def handle_unselected(self):
+        """
+        Occurs when the application is unselected (as another app is being selected)
+        """
         self.select_page(None)
 
     def handle_selected(self):
+        """
+        Occurs when the application is being selected
+        """
         self.select_page(self.get_default_page())
 
     def handle_reselected(self):
+        """
+        Occurs when the user selects the application while the application is already selected. This is useful in
+        some cases for special button handling.
+        """
         self.select_page(self.get_default_page())
 
+    # noinspection PyMethodMayBeStatic
     def page_reselected(self, page):
+        """
+        Occurs when a page in this application is reselected (selected while already selected)
+        :type page: MFDPage The page being reselected
+        """
         pass
 
     def select_page_by_index(self, index):
+        """
+        Selects the specified page in the application by its 0-based index
+        :type index: int The 0-based index of the page we selected.
+        """
 
         # Figure out what we clicked
         if index < len(self.pages):
@@ -52,6 +90,10 @@ class MFDApplication(object):
         self.select_page(page)
 
     def select_page(self, page):
+        """
+        Selects the specified page
+        :type page: The MFDPage in this application
+        """
 
         # Don't allow switching to a "none" page
         if page is None:
@@ -75,6 +117,11 @@ class MFDApplication(object):
 
 class PlaceholderApp(MFDApplication):
 
+    """
+    A placeholder application representing content not implemented yet. Built for easier prototyping.
+    :type controller: PiMFD.Controller.MFDController T he controller
+    :type label: str The button label for the application.
+    """
     button_text = 'UNKN'
 
     def __init__(self, controller, label):
@@ -82,4 +129,8 @@ class PlaceholderApp(MFDApplication):
         self.button_text = label
 
     def get_button_text(self):
+        """
+        Gets the text used to render the button
+        :return: the text used to render the button
+        """
         return self.button_text
