438ef8f4aa0a1338669db407af69d343304cad51
Matt Eland <Matt.Eland@Outlook.com>
2015-01-15 23:47:03 -0500

    Finished documenting the remaining classes. Yaaaaaay for documentation!

--- PiMFD/Applications/MFDPage.py
+++ PiMFD/Applications/MFDPage.py
@@ -1,7 +1,17 @@
+# coding=utf-8
+"""
+Defines the MFDPage used as a root for other pages in the application.
+"""
+
 __author__ = 'Matt Eland'
 
 
 class MFDPage(object):
+    """
+    Represents a page within the application
+    :type controller: PiMFD.Controller.MFDController The controller
+    :type application: The application containing this page.
+    """
     top_headers = list()
     bottom_headers = list()
 
@@ -15,22 +25,42 @@ class MFDPage(object):
         self.application = application
 
     def handle_unselected(self):
+        """
+        Occurs when a page was the selected page within an application but no longer is the selected page.
+        """
         pass
 
     def handle_selected(self):
+        """
+        Occurs when a page was selected but another page was selected instead, causing this page to be unselected.
+        """
         pass
 
     def handle_reselected(self):
+        """
+        If the page is selected but the user is trying to navigate to it anyway, retrigger and refresh weather data.
+        """
         self.application.page_reselected(self)
 
     def get_button_text(self):
+        """
+        Gets the button text
+        :return: The button text
+        """
         return 'UNKN'
 
     def render(self, display):
+        """
+        Handles rendering the page.
+        :type display: PiMFD.DisplayManager.DisplayManager The display manager.
+        """
         pass
 
 
 class SimpleMessagePage(MFDPage):
+    """
+    Represents a simple single-message page, defaulting to 'not implemented'.
+    """
 
     button_text = "NI"
     message = "Not Implemented"
@@ -41,9 +71,17 @@ class SimpleMessagePage(MFDPage):
         self.message = message
 
     def get_button_text(self):
+        """
+        Gets the text for a button
+        :return: The button text
+        """
         return self.button_text
 
     def render(self, display):
+        """
+        Renders a simple message page.
+        :type display: PiMFD.DisplayManager.DisplayManager The DisplayManager
+        """
         super(SimpleMessagePage, self).render(display)
 
         display.render_text_centered(self.display.font_normal,
--- PiMFD/ColorScheme.py
+++ PiMFD/ColorScheme.py
@@ -1,7 +1,19 @@
+# coding=utf-8
+"""
+Color Scheme information
+"""
 __author__ = 'Matt Eland'
 
 
 class ColorScheme(object):
+    """
+    A color scheme
+    :type background: tuple RGB values indicating the background
+    :type foreground: tuple RGB values for most text and buttons
+    :type highlight: tuple RGB values for highlighted text
+    :type interlace_color: tuple RGB values indicating the alternate rows of the background or None for no interlacing.
+    """
+
     def __init__(self, background=(0, 0, 0), foreground=(0, 255, 0), highlight=(255, 255, 255), interlace_color=(2, 2, 2)):
         self.background = background
         self.foreground = foreground
@@ -10,6 +22,11 @@ class ColorScheme(object):
         pass
 
     def clone_to(self, target):
+        """
+        Clones values in this object to other objects
+        :param target: The object to receive the values
+        :return: The target with its adjusted files.
+        """
         target.background = self.background
         target.foreground = self.foreground
         target.highlight = self.highlight
@@ -25,6 +42,11 @@ class ColorScheme(object):
 
 
 class ColorSchemes(object):
+    """
+    A collection of available color schemes.
+    """
+
+    # TODO: Stick these in a list so I can iterate it and store a color scheme preference in the settings file.
 
     # A green based color scheme resembling military avionics displays
     military = ColorScheme(background=(0, 8, 0), foreground=(0, 255, 0), highlight=(255, 255, 255), interlace_color=(0, 32, 0))
