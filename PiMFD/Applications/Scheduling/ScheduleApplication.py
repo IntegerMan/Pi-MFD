826da898c336d5cbf60ca23cba95fc8cd25d5f6d
Matt Eland <Matt.Eland@Outlook.com>
2015-01-15 23:26:29 -0500

    Documentation, refreshing weather.

--- PiMFD/Applications/Scheduling/ScheduleApplication.py
+++ PiMFD/Applications/Scheduling/ScheduleApplication.py
@@ -1,3 +1,8 @@
+# coding=utf-8
+"""
+The scheduling application
+"""
+
 from PiMFD.Applications.Application import MFDApplication
 from PiMFD.Applications.MFDPage import SimpleMessagePage
 from PiMFD.Applications.Scheduling.Weather.WeatherAPIWrapper import WeatherAPI
@@ -8,7 +13,10 @@ __author__ = 'Matt Eland'
 
 
 class ScheduleApp(MFDApplication):
-
+    """
+    The scheduling application. Contains pages related to scheduling and coordinates web-service communication.
+    :type controller: PiMFD.Controller.MFDController The controller.
+    """
     root_page = None
     task_page = None
     mail_page = None
@@ -31,18 +39,54 @@ class ScheduleApp(MFDApplication):
 
         self.pages = list([self.task_page, self.mail_page, self.calendar_page, self.weather_page])
 
-        # TODO: There's no mechanism to refresh weather data. It probably should do so either automatically 
-        #       at interval or the first time the sch. app or weather has been activated in the last X minutes.
+        # TODO: There's no automatic mechanism to refresh weather data.
         self.get_weather()
 
     def get_weather(self):
-        self.weather_data = self.weather_api.get_yahoo_weather(self.controller.location)
+        """
+        Gets weather data from the weather API (Yahoo Weather) and stores it for the weather page.
+        """
+
+        # noinspection PyBroadException
+        try:
+            # TODO: I might want to impose some frequency restrictions here
+            self.weather_data = self.weather_api.get_yahoo_weather(self.controller.location)
+        except Exception as exception:
+            self.weather_data.last_result = 'Could not get weather: ' + exception.message
 
     def get_default_page(self):
+        """
+        Gets the default page within the application.
+        :return: the default page within the application.
+        """
         return self.root_page
 
     def get_button_text(self):
+        """
+        Gets text for the button representing this page.
+        :return: the button text for the application button.
+        """
         return 'SCH'
+
+    def page_reselected(self, page):
+        """
+        Handles the page reselected event for this application.
+        :param page: The page that was reselected.
+        """
+        super(ScheduleApp, self).page_reselected(page)
+
+        # If the user re-selects weather, refresh data
+        if page is self.weather_page:
+            self.get_weather()
+
+    def handle_selected(self):
+        """
+        Handles the selected event for this application.
+        """
+
+        # Fetch weather data!
+        self.get_weather()
+
 
 
 
