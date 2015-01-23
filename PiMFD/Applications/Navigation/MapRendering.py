00444d82d887bfe0794999e31969e91e9238ee51
Matt Eland <Matt.Eland@Outlook.com>
2015-01-23 00:08:28 -0500

    Dynamic centering of map data.

--- PiMFD/Applications/Navigation/MapRendering.py
+++ PiMFD/Applications/Navigation/MapRendering.py
@@ -19,8 +19,10 @@ class MapRenderer(object):  # TODO: Maybe this should be a UIWidget?
         offset = self.display.get_content_start_pos()
 
         available_y = self.display.res_y - (offset[1] * 2)
+        size = (available_y, available_y)
+        center = ((self.display.res_x / 2.0) - 10, (self.display.res_y / 2.0) - 10)
 
-        ways = self.map.transpose_ways((available_y, available_y), (390, 225))
+        ways = self.map.transpose_ways(size, center)
 
         for way in ways:
             # TODO: Use the rendering helpers
