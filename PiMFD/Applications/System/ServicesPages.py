d71cf4bd09684d40885f84c482ba547d39d35d56
Matt Eland <Matt.Eland@Outlook.com>
2015-02-09 21:52:35 -0500

    PIMFD-83 - List Running Services
    Getting some bones to things, now.

--- PiMFD/Applications/System/ServicesPages.py
+++ PiMFD/Applications/System/ServicesPages.py
@@ -4,13 +4,26 @@
 Lists running services on this machine using WMI
 """
 from PiMFD.Applications.MFDPage import MFDPage
+from PiMFD.UI.Panels import StackPanel
 
 __author__ = 'Matt'
 
 
 class ServicesPage(MFDPage):
+    """
+    A page containing information on running services
+    """
+
+    services = list()
+
     def __init__(self, controller, application):
         super(ServicesPage, self).__init__(controller, application)
+
+        self.services = list()
+
+        self.lbl_header = self.get_header_label('RUNNING SERVICES')
+        self.pnl_services = StackPanel(controller.display, self)
+        self.panel.children = [self.lbl_header, self.pnl_services]
 
     def handle_unselected(self):
         super(ServicesPage, self).handle_unselected()
@@ -21,8 +34,15 @@ class ServicesPage(MFDPage):
     def handle_selected(self):
         super(ServicesPage, self).handle_selected()
 
-    def render(self):
-        self.center_text("NO DATA", self.display.color_scheme.highlight)
+        self.refresh_services()
 
-        return super(ServicesPage, self).render()
+    def refresh_services(self):
+        pass
+
+    def render(self):
+
+        if not self.services or len(self.services) <= 0:
+            self.center_text("NO SERVICE DATA AVAILABLE", self.display.color_scheme.highlight)
+        else:
+            return super(ServicesPage, self).render()
 
