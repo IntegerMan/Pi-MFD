ea67b1fd8e84b9b5d8889c413c218d444a64a73d
Matt Eland <Matt.Eland@Outlook.com>
2015-02-12 18:27:44 -0500

    PIMFD-134 - Drill into individual disks for details
    Documentation for existing disk drive page.

--- PiMFD/Applications/System/DiskPages.py
+++ PiMFD/Applications/System/DiskPages.py
@@ -1,4 +1,9 @@
 # coding=utf-8
+
+"""
+Contains Disk Drive Pages
+"""
+
 try:
     import psutil
 except ImportError:
@@ -12,7 +17,12 @@ __author__ = 'Matt Eland'
 
 
 class DiskDrivesPage(MFDPage):
-
+    """
+    A page displaying a list of disk drives on this machine.
+    :type controller: PiMFD.Controller.MFDController
+    :type application: PiMFD.Applications.System.SystemApplication.SysApplication
+    :type auto_scroll: bool
+    """
     partitions = None
 
     def __init__(self, controller, application, auto_scroll=True):
@@ -20,8 +30,12 @@ class DiskDrivesPage(MFDPage):
 
         self.refresh()
 
-    def refresh(self,):
+    def refresh(self):
 
+        """
+        Refreshes the list of drives
+        """
+        
         if not psutil:
             self.partitions = None
             return
@@ -52,28 +66,44 @@ class DiskDrivesPage(MFDPage):
                 text = "{} {} {} ({} % Full)".format(drive, file_system, options, percent)
 
             lbl = MenuItem(self.controller.display, self, text)
+            lbl.font = self.controller.display.fonts.list
             self.panel.children.append(lbl)
 
             if is_first_control:
                 self.set_focus(lbl)
                 is_first_control = False
 
-
     def arrange(self):
+        """
+        Arranges the control to the page
+        :return: The desired size of the page
+        """
         return super(DiskDrivesPage, self).arrange()
 
     def render(self):
 
+        """
+        Renders the control to the screen
+        :return: The rectangle of the control
+        """
         if not psutil:
             self.center_text("psutil offline".upper())
 
         return super(DiskDrivesPage, self).render()
 
     def handle_reselected(self):
+        """
+        Handles the reselected event
+        """
         self.refresh()
         super(DiskDrivesPage, self).handle_reselected()
 
     def get_button_text(self):
+        """
+        Gets the text for this page's button
+        :return: The text for this page's button
+        :rtype: str
+        """
         return "DRVS"
 
 
