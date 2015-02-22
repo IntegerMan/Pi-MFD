31dedbd238f6fa607876a34bce03b0d5d3932160
Matt Eland <Matt.Eland@Outlook.com>
2015-02-22 17:34:50 -0500

    PIMFD-155 - Alert for adverse current weather temperature

--- PiMFD/Applications/Scheduling/Weather/WeatherDataProvider.py
+++ PiMFD/Applications/Scheduling/Weather/WeatherDataProvider.py
@@ -4,7 +4,7 @@
 This file contains data provider for weather information
 """
 from PiMFD.DataProvider import DataProvider
-from PiMFD.UI.Widgets.DashboardWidget import TextDashboardWidget
+from PiMFD.UI.Widgets.DashboardWidget import TextDashboardWidget, DashboardStatus
 
 __author__ = 'Matt Eland'
 
@@ -24,10 +24,22 @@ class WeatherDataProvider(DataProvider):
 
     def get_dashboard_widgets(self, display, page):
 
+        weather_status = DashboardStatus.Passive
+
         if self.weather_data:
             weather_text = u"{}{} {}".format(self.weather_data.temperature, self.weather_data.temp_units, self.weather_data.conditions)
+            
+            # Certain Temperatures should function as alerts
+            numeric_temp = float(self.weather_data.temperature)
+            if numeric_temp <= 10 or numeric_temp >= 100:
+                weather_status = DashboardStatus.Critical
+            elif numeric_temp <= 32 or numeric_temp >= 90:
+                weather_status = DashboardStatus.Caution
+                
         else:
-            weather_text = "Loading..."
+            # We may be loading
+            weather_text = "Offline"
+            weather_status = DashboardStatus.Inactive
 
         if not self.current_conditions_widget:
             # Build out the widget
@@ -35,5 +47,7 @@ class WeatherDataProvider(DataProvider):
         else:
             # Update with current system time
             self.current_conditions_widget.value = weather_text
+            
+        self.current_conditions_widget.status = weather_status
 
         return [self.current_conditions_widget]
