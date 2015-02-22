9f63e11361489f53752f94443e2d6dd28bee7059
Matt Eland <Matt.Eland@Outlook.com>
2015-02-22 16:32:27 -0500

    PIMFD-125 - Add Alerts Dashboard
    Added border around dashboard widgets.

--- PiMFD/UI/Widgets/DashboardWidget.py
+++ PiMFD/UI/Widgets/DashboardWidget.py
@@ -3,14 +3,26 @@
 """
 This file contains dashboard widgets
 """
+from pygame.rect import Rect
+
 from PiMFD.UI.Panels import StackPanel
+from PiMFD.UI.Rendering import render_rectangle
 from PiMFD.UI.Text import TextBlock
 from PiMFD.UI.WidgetBase import UIWidget
+
 
 __author__ = 'Matt Eland'
 
 
 class TextDashboardWidget(UIWidget):
+    """
+    A simple labeled dashboard widget
+    :type display: PiMFD.UI.DisplayManager.DisplayManager
+    :type page: PiMFD.Applications.Core.DashboardPages.DashboardPage
+    :type title: str The name of the widget
+    :type value: str The value used in the widget
+    """
+
     def __init__(self, display, page, title, value):
         super(TextDashboardWidget, self).__init__(display, page)
 
@@ -28,7 +40,18 @@ class TextDashboardWidget(UIWidget):
         self.panel.children.append(self.lbl_value)
 
     def render(self):
-        return self.panel.render_at(self.pos)
+        # Render the base content with some padding
+        padding = 8
+        pos = self.pos[0] + padding, self.pos[1] + padding
+        content_rect = self.panel.render_at(pos)
+
+        # Render an outline around the entire control
+        rect = Rect(self.pos[0], self.pos[1], content_rect.width + (padding * 2), content_rect.height + (padding * 2))
+        color = self.display.color_scheme.foreground
+        render_rectangle(self.display, color, rect)
+
+        # Assume the width of the outer outline
+        return self.set_dimensions_from_rect(rect) 
 
     def arrange(self):
         self.lbl_title.text = self.title
