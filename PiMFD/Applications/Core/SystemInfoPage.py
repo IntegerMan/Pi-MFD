f5aabdb6225ef4abb17e26bf3ccfb5c45ea60966
Matt Eland <Matt.Eland@Outlook.com>
2015-03-08 01:42:38 -0500

    Processor optional, use list for info page.

--- PiMFD/Applications/Core/SystemInfoPage.py
+++ PiMFD/Applications/Core/SystemInfoPage.py
@@ -18,14 +18,14 @@ class SysInfoPage(MFDPage):
         super(SysInfoPage, self).__init__(controller, application)
 
         self.lbl_app_header = self.get_header_label("{} Information")
-        self.lbl_app_version = self.get_label("   Ver: {}")
-        self.lbl_app_legal = self.get_label(" Legal: Copyright (c) {} {}")
+        self.lbl_app_version = self.get_list_label("   Ver: {}")
+        self.lbl_app_legal = self.get_list_label(" Legal: Copyright (c) {} {}")
         lbl_sys_header = self.get_header_label("System Information")
-        self.lbl_sys_name = self.get_label("System: {} {} {}")
-        self.lbl_sys_processor = self.get_label("  Proc: {}")
-        self.lbl_sys_net_id = self.get_label("Net ID: {}")
-        self.lbl_sys_display = self.get_label("  Disp: {}x{}")
-        self.lbl_sys_python = self.get_label("Python: {} {} {}")
+        self.lbl_sys_name = self.get_list_label("System: {} {} {}")
+        self.lbl_sys_processor = self.get_list_label("  Proc: {}")
+        self.lbl_sys_net_id = self.get_list_label("Net ID: {}")
+        self.lbl_sys_display = self.get_list_label("  Disp: {}x{}")
+        self.lbl_sys_python = self.get_list_label("Python: {} {} {}")
 
         self.panel.children = (
             self.lbl_app_header,
@@ -56,6 +56,7 @@ class SysInfoPage(MFDPage):
         self.lbl_app_legal.text_data = opts.app_author, opts.copyright_year
         self.lbl_sys_name.text_data = platform.platform(), platform.release(), platform.machine()
         self.lbl_sys_processor.text_data = platform.processor()
+        self.lbl_sys_processor.visible = self.lbl_sys_processor.text_data
         self.lbl_sys_net_id.text_data = platform.node()
         self.lbl_sys_display.text_data = self.display.bounds.right, self.display.bounds.bottom
         self.lbl_sys_python.text_data = platform.python_version(), platform.python_implementation(), platform.python_compiler()
