b875f8612311074fbe8c02ab03bdbebec425ba36
Matt Eland <Matt.Eland@Outlook.com>
2015-03-08 01:36:53 -0500

    Removed dashboard label to make more room for display on RPI.

--- PiMFD/Applications/Core/DashboardPages.py
+++ PiMFD/Applications/Core/DashboardPages.py
@@ -4,7 +4,7 @@
 This file contains dashboard pages
 """
 from PiMFD.Applications.MFDPage import MFDPage
-from PiMFD.UI.Panels import StackPanel, DoubleWideStackPanel, WrapPanel
+from PiMFD.UI.Panels import WrapPanel
 
 __author__ = 'Matt Eland'
 
@@ -17,13 +17,11 @@ class DashboardPage(MFDPage):
     def __init__(self, controller, application, time_data_provider):
         super(DashboardPage, self).__init__(controller, application)
 
-        header = self.get_header_label("Pi-MFD System Dashboard")
-
         self.pnl_alerts = WrapPanel(controller.display, self)
 
         self.time_data_provider = time_data_provider
 
-        self.panel.children = [header, self.pnl_alerts]
+        self.panel.children = [self.pnl_alerts]
 
     def get_button_text(self):
         """
